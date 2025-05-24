2e268308d971ba85c0c2ed527c3c63065d522b4b
Bretny Bal-ut <abonbretny@gmail.com>
2025-05-24 20:55:28 +0800

    lq1.3_Bal-ut.js

---
+++ lq1.3_Bal-ut.js
@@ -0,0 +1,9 @@
+let fname = "Jose Cruz";
+let i = 0;
+
+while (i < fname.length) {
+    let c = 2 + i;
+    let nLengthCounterTwo = fname.length - c;
+    console.log(fname);
+    console.log("Iterations Name was Printed: " + i);
+    i++;
